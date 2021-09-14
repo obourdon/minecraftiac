f82274cc631ef857d8fd9d7079593df8951b0b95
quizforparty44 <quizforparty44@gmail.com>
2021-09-14 16:08:22 +0200

    show version

---
+++ showtoolsversion.sh
@@ -0,0 +1,17 @@
+#Tools
+jq --version
+unzip -v
+curl --version
+wget --version
+#Terraform
+terraform --version
+#Vault
+vault -v
+#Java
+java -version
+#Python
+python3 --version
+#Ansible
+ansible --version
+#OCI CLI
+oci --version
